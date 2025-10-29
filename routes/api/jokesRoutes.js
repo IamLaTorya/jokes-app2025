793bcf80796ece72e10882bb617a64e8fbc15cb5
IamLaTorya <LaTorya.Sadler@yahoo.com>
2025-10-28 21:52:15 -0500

    added new code and pages

--- routes/api/jokesRoutes.js
+++ routes/api/jokesRoutes.js
@@ -1,22 +1,61 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const { paginationResults, buildJokeArr } = require('../../helpers/pagination')
 const PORT = process.env.PORT || 3001
 
 // http://localhost:3001/jokes
 router.get('/', (req, res)=> {
     // res.send('This works')
     const url = `https://api.sampleapis.com/jokes/goodJokes`
+    /** pagination...🤞🏾 */
+    const pageData = paginationResults(req)
+
+    // will store jokes in here...
+    let jokesArr = []
 
     axios.get(url)
         .then(resp => {
+
+            const jokeArrData = buildJokeArr(resp.data, jokesArr, pageData.startIdx, pageData.endIdx, pageData.page )
+
             res.render('pages/allJokes', {
                 title: 'All Jokes',
                 name: 'All Jokes',
-                data: resp.data
+                data: jokeArrData.arr,
+                prev: jokeArrData.prev,
+                next: jokeArrData.next
             })
         })
+})
 
+// Joke Type
+// localhost:3001/jokes/type/:type
+router.get('/type/:type', (req, res)=> {
+
+    const type = req.params.type
+    const url = `https://api.sampleapis.com/jokes/goodJokes`
+    const pageData = paginationResults(req)
+
+    // We will filter through resp.data and store in typeArr
+    let typeArr = []
+
+    let jokesArr = []
+
+    axios.get(url)
+        .then(resp => typeArr = resp.data.filter(item => item.type == type))
+        .then(typeArr => {
+
+            const jokeArrData = buildJokeArr(typeArr, jokesArr, pageData.startIdx, pageData.endIdx, pageData.page)
+
+            res.render('pages/allJokes', {
+                title: type,
+                name: `${type} jokes`,
+                data: jokeArrData.arr,
+                prev: jokeArrData.prev,
+                next: jokeArrData.next
+            })
+        })
 })
 
 module.exports = router
